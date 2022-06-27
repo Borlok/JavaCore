94ae1791186ab611afc80bc421da9b4da5c92b4d
Yurii Erofeevskyi <yurii.erofeevskyi@talpabox.com>
2022-06-27 09:47:29 +0300

    add Covariant collections

--- src/main/java/com/borlok/javacore/generic/Main.java
+++ src/main/java/com/borlok/javacore/generic/Main.java
@@ -1,7 +1,9 @@
 package main.java.com.borlok.javacore.generic;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * <? extends Type> only reading and add(null)
@@ -14,6 +16,12 @@ public class Main {
         new GenericClassDemo<Integer>().sum(integerList); // Generic class as a Number subtype
         new GenericClassDemo<Number>().sumWithAnythingType(integerList); // Generic class as a <? extends Number> subtype in the method arguments
         System.out.println(new GenericClassDemo<Double>().getNum(List.of(1.0,2.0,3.0), Double.class)); // Demo with returned T type
-        new GenericClassDemo<Integer>().copyArray(List.of(1,2,3), new ArrayList<>());//
+        new GenericClassDemo<Integer>().copyArray(List.of(1,2,3), new ArrayList<>());// Demo reading and writing generic Type
+        /// Covariant collection with generic////
+        Set<Integer> set = new HashSet<>();
+        Set<? extends Number> covariantSet = set;
+        /// Invariant collection ///
+        Set<Integer> integers = new HashSet<>();
+//        Set<Number> numbers = integers; // Compile Error
     }
 }
